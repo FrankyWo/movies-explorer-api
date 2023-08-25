d6118169a8c38b7262258456ab1c3294681c5ea5
Ilya Kubenko <i.kubenko@mail.ru>
2023-08-25 18:36:29 +0800

    исправления auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,18 +4,21 @@ const { JWT_SECRET } = require('../utils/config');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходимо авторизироваться'));
+  }
+
   const token = authorization.replace('Bearer ', '');
+
   let payload;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) return next(new UnauthorizedError('Необходимо авторизироваться'));
-
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new UnauthorizedError('Необходимо авторизироваться'));
+    return next(new UnauthorizedError('Необходимо авторизироваться'));
   }
-  req.user = payload;
 
+  req.user = payload;
   return next();
 };
 
