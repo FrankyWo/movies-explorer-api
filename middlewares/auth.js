142f6f838f7933837f0e4970376e7d6502ec505d
Ilya Kubenko <i.kubenko@mail.ru>
2023-08-25 18:41:35 +0800

    auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,26 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError');
-const { JWT_SECRET } = require('../utils/config');
+const AuthorizationError = require('../errors/UnauthorizedError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходимо авторизироваться'));
+    return next(new AuthorizationError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
-
   let payload;
+
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    return next(new UnauthorizedError('Необходимо авторизироваться'));
+    next(new AuthorizationError('Необходима авторизация'));
   }
 
   req.user = payload;
+
   return next();
 };
-
 module.exports = auth;
