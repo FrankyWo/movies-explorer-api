1f65e8fc09337a02b53b44bc58b8966ab7b6a15c
Ilya Kubenko <i.kubenko@mail.ru>
2023-09-03 14:21:06 +0800

    issue cookieParser

--- utils/token.js
+++ utils/token.js
@@ -10,13 +10,13 @@ const { JWT_SECRET } = require('../configs');
  * @param {Payload} payload
  * @returns {string} token
  */
-const generateToken = (payload) => jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' });
+const generateToken = (payload) => jwt.sign(payload, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'secret', { expiresIn: '7d' });
 
 /**
  * @param {string} token
  * @returns { Payload | Error} token payload or Error
  */
-const verifyToken = (token) => jwt.verify(token, JWT_SECRET);
+const verifyToken = (token) => jwt.verify(token, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'secret',);
 
 module.exports = {
   generateToken,
