f5bc295d58ecbeafe63d3748aff3b4ba30636687
Ilya Kubenko <i.kubenko@mail.ru>
2023-09-03 15:02:05 +0800

    +env

--- configs/index.js
+++ configs/index.js
@@ -2,10 +2,12 @@ const {
   PORT = 3000,
   BD_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
   JWT_SECRET = 'secret',
+  NODE_ENV = 'production',
 } = process.env;
 
 module.exports = {
   PORT,
   BD_URL,
   JWT_SECRET,
+  NODE_ENV,
 };
