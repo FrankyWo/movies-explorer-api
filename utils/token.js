ed38cccc5ad81de0a6de4d5b724ff9b9d9e7d042
Ilya Kubenko <i.kubenko@mail.ru>
2023-09-03 14:32:31 +0800

    auth issue 401

--- utils/token.js
+++ utils/token.js
@@ -16,7 +16,7 @@ const generateToken = (payload) => jwt.sign(payload, process.env.NODE_ENV === 'p
  * @param {string} token
  * @returns { Payload | Error} token payload or Error
  */
-const verifyToken = (token) => jwt.verify(token, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'secret',);
+const verifyToken = (token) => jwt.verify(token, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'secret');
 
 module.exports = {
   generateToken,
