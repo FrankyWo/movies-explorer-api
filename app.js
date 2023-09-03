8205f6f89b9c991e27618a22cb8ab992e2bb88d7
Ilya Kubenko <i.kubenko@mail.ru>
2023-09-03 13:27:21 +0800

    another one dif back logic

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const {
 } = require('./middlewares');
 
 const app = express();
+app.use(cookieParser());
 mongoose.connect(BD_URL, {
   useNewUrlParser: true,
 });
@@ -21,7 +22,6 @@ app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.use(requestLogger);
 app.use(handleCors);
