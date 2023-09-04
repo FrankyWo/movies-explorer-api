84d3b6af54457d48b8d69389abfec0340dc124db
Ilya Kubenko <i.kubenko@mail.ru>
2023-09-05 05:16:25 +0800

    fix+5

--- src/routes/signup.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { createValidation } = require('../user/validation/validation');
-const { createUser } = require('../user/controller/userController');
-
-router.post('/signup', createValidation, createUser);
-
-module.exports = router;
